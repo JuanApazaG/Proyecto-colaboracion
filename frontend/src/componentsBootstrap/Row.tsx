902533e042dd74af2ff3e54d4151c0e81173d4b9
Fernando <fernandoarroyo0011@gmail.com>
2022-09-08 20:25:04 -0400

    Listo para iniciar

---
+++ frontend/src/componentsBootstrap/Row.tsx
@@ -0,0 +1,14 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+function ContainerExample() {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 1 Hello</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default ContainerExample;
